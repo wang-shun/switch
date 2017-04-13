6eebd8112d3ddeffb47717cb89e1bd129fd289c0
xiezg <xiezg@317hu.com>
2017-04-13 17:21:41 +0800

    开关登录页暂存

--- switch-web/src/main/java/com/bozhong/myswitch/domain/SwitchValueChangDO.java
+++ switch-web/src/main/java/com/bozhong/myswitch/domain/SwitchValueChangDO.java
@@ -2,9 +2,110 @@ package com.bozhong.myswitch.domain;
 
 /**
  * Created by renyueliang on 17/4/12.
+ * 数据更新实体类
  */
 public class SwitchValueChangDO extends BaseDO {
+    /**
+     * 操作ID
+     */
+    private String optId;
 
+    /**
+     * IP
+     */
+    private String ip;
 
+    /**
+     * 字段名称
+     */
+    private String fieldName;
 
+    /**
+     * 字段值
+     */
+    private String fieldValue;
+
+    /**
+     * 操作人
+     */
+    private String createBy;
+
+    /**
+     * 操作时间
+     */
+    private String createDt;
+
+    /**
+     * 回调时间
+     */
+    private String callbackDT;
+
+    /**
+     * 同步结果（成功或者事变）
+     */
+    private String syncResult;
+
+    public String getOptId() {
+        return optId;
+    }
+
+    public void setOptId(String optId) {
+        this.optId = optId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(String createDt) {
+        this.createDt = createDt;
+    }
+
+    public String getCallbackDT() {
+        return callbackDT;
+    }
+
+    public void setCallbackDT(String callbackDT) {
+        this.callbackDT = callbackDT;
+    }
+
+    public String getSyncResult() {
+        return syncResult;
+    }
+
+    public void setSyncResult(String syncResult) {
+        this.syncResult = syncResult;
+    }
 }
---
+++ switch-web/src/main/java/com/bozhong/myswitch/view/manager/module/screen/Login.java
@@ -0,0 +1,39 @@
+package com.bozhong.myswitch.view.manager.module.screen;
+
+import com.bozhong.config.util.CookiesUtil;
+import com.bozhong.config.util.LDAPUtil;
+import com.bozhong.myredis.MyRedisClusterForHessian;
+import com.yx.eweb.main.EWebContext;
+import com.yx.eweb.main.ScreenInter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by xiezg@317hu.com on 2016/12/30 0030.
+ */
+@Controller
+public class Login implements ScreenInter {
+
+    @Autowired
+    private MyRedisClusterForHessian myRedisClusterForHessian;
+    @Override
+    public void excute(EWebContext eWebContext) {
+        System.out.println("Login当前线程：" + Thread.currentThread().getName());
+        HttpServletRequest request = eWebContext.getRequest();
+        HttpServletResponse response = eWebContext.getResponse();
+        Cookie tokenCookie = CookiesUtil.getCookieByName(request, "token");
+        if (tokenCookie != null) {
+            String token = tokenCookie.getValue();
+            tokenCookie = new Cookie("token", null);
+            tokenCookie.setPath("/");
+            tokenCookie.setMaxAge(0);
+            response.addCookie(tokenCookie);
+            myRedisClusterForHessian.delForStr(LDAPUtil.CONFIG_CENTER_USERNAME_PREFIX+token);
+        }
+
+    }
+}
