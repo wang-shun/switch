df02af77977a1e008c83a60dc268441f08b87c99
xiezg <xiezg@317hu.com>
2017-04-14 08:23:42 +0800

    开关平台主体页面开发

---
+++ switch-web/src/main/java/com/bozhong/myswitch/view/manager/module/screen/DataChangeView.java
@@ -0,0 +1,16 @@
+package com.bozhong.myswitch.view.manager.module.screen;
+
+import com.yx.eweb.main.EWebContext;
+import com.yx.eweb.main.ScreenInter;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by xiezg@317hu.com on 2017/4/14 0014.
+ */
+@Controller
+public class DataChangeView implements ScreenInter {
+    @Override
+    public void excute(EWebContext eWebContext) {
+        eWebContext.put("menu", this.getClass().getSimpleName());
+    }
+}
